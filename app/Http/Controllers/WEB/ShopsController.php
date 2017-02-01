0cc594cdf4f9eaf200742117359b3554058b5732
Pedro Luna <neko.hitokori@gmail.com>
2017-01-31 18:49:53 -0600

    pre fix

--- app/Http/Controllers/WEB/ShopsController.php
+++ app/Http/Controllers/WEB/ShopsController.php
@@ -4,16 +4,40 @@ namespace App\Http\Controllers\WEB;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
+use Validator;
 
 class ShopsController extends Controller
 {
+
+    /**
+     * Vista general de comercios para administrador
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         return view('shops.index');
     }
 
+    /**
+     * Formulario para nuevo comercio
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
         return view('shops.create');
+    }
+
+
+    public function store()
+    {
+        $validator = Validator::make(Input::all(), [
+            'name' => 'required|',
+            'category' => 'required|',
+        ]);
+        if ($validator->fails())
+            return redirect()->route('shops.create')->withErrors($validator);
+
+        n
     }
 }

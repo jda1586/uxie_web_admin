0c45df9572486da4d596c6f1a3e26815478067b9
asdrubal <jose_asdrubal1@hotmail.com>
2017-03-07 00:38:17 -0600

    modificaciones a la agenda

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -36,7 +36,7 @@
     <div id="full-calendar">
         <div class="row">
             <div class="col s12 m4 l3">
-                <div id='external-events'>
+                {{--<div id='external-events'>
                     <h4 class="header">Draggable Events</h4>
                     <div class='fc-event cyan'>March Invoices</div>
                     <div class='fc-event teal'>Call Emy</div>
@@ -49,7 +49,7 @@
                         <input type='checkbox' id='drop-remove' />
                         <label for='drop-remove'>remove after drop</label>
                     </p>
-                </div>
+                </div>--}}
             </div>
             <div class="col s12 m8 l9">
                 <div id='calendar'></div>
@@ -61,10 +61,108 @@
 @section('_footer')
     @parent
 
-    {{--<script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/lib/jquery.min.js') !!}"></script>--}}
-    <script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/lib/jquery-ui.custom.min.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/new/moment2.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/new/fullcalendar.min.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/new/locale-all.js') !!}"></script>
+    <script>
+
+
+        $(document).ready(function() {
+            var initialLocaleCode = 'es';
+
+            $('#calendar').fullCalendar({
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay,listMonth'
+                },
+                defaultDate: '2017-02-12',
+                locale: initialLocaleCode,
+                buttonIcons: false, // show the prev/next text
+                weekNumbers: true,
+                navLinks: true, // can click day/week names to navigate views
+                editable: true,
+                eventLimit: true, // allow "more" link when too many events
+                events: [
+                    {
+                        title: 'All Day Event',
+                        start: '2017-02-01'
+                    },
+                    {
+                        title: 'Long Event',
+                        start: '2017-02-07',
+                        end: '2017-02-10'
+                    },
+                    {
+                        id: 999,
+                        title: 'Repeating Event',
+                        start: '2017-02-09T16:00:00'
+                    },
+                    {
+                        id: 999,
+                        title: 'Repeating Event',
+                        start: '2017-02-16T16:00:00'
+                    },
+                    {
+                        title: 'Conference',
+                        start: '2017-02-11',
+                        end: '2017-02-13'
+                    },
+                    {
+                        title: 'Meeting',
+                        start: '2017-02-12T10:30:00',
+                        end: '2017-02-12T12:30:00'
+                    },
+                    {
+                        title: 'Lunch',
+                        start: '2017-02-12T12:00:00'
+                    },
+                    {
+                        title: 'Meeting',
+                        start: '2017-02-12T14:30:00'
+                    },
+                    {
+                        title: 'Happy Hour',
+                        start: '2017-02-12T17:30:00'
+                    },
+                    {
+                        title: 'Dinner',
+                        start: '2017-02-12T20:00:00'
+                    },
+                    {
+                        title: 'Birthday Party',
+                        start: '2017-02-13T07:00:00'
+                    },
+                    {
+                        title: 'Click for Google',
+                        url: 'http://google.com/',
+                        start: '2017-02-28'
+                    }
+                ]
+            });
+
+            // build the locale selector's options
+            $.each($.fullCalendar.locales, function(localeCode) {
+                $('#locale-selector').append(
+                    $('<option/>')
+                        .attr('value', localeCode)
+                        .prop('selected', localeCode == initialLocaleCode)
+                        .text(localeCode)
+                );
+            });
+
+            // when the selected option changes, dynamically change the calendar option
+            $('#locale-selector').on('change', function() {
+                if (this.value) {
+                    $('#calendar').fullCalendar('option', 'locale', this.value);
+                }
+            });
+        });
+
+    </script>
+    {{--<script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/lib/jquery-ui.custom.min.js') !!}"></script>
     <script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/lib/moment.min.js') !!}"></script>
     <script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/js/fullcalendar.min.js') !!}"></script>
-    <script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/fullcalendar-script.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/fullcalendar-script.js') !!}"></script>--}}
 
 @endsection

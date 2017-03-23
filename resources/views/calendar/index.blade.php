1142c3f87528ee1034970702007a93d770aaa4ce
asdrubal <jose_asdrubal1@hotmail.com>
2017-03-22 21:54:22 -0600

    modificaciones a la agenda

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -17,13 +17,13 @@
                 <div class="col s12 m12 l12">
                     <h5 class="breadcrumbs-title">Agenda </h5>
                     <ol class="breadcrumbs">
-                        {{--<li>
+                        <li>
                             <a href="{!! route('home') !!}">Tablero</a>
                         </li>
-                        <li>
+                        {{--<li>
                             <a href="{!! route('shops.index') !!}">Comercios</a>
                         </li>--}}
-                        {{--<li class="active">Agenda</li>--}}
+                        <li class="active">Agenda</li>
                     </ol>
                 </div>
             </div>
@@ -33,29 +33,20 @@
 
 @section('content')
     <div class="divider"></div>
-    <div id="full-calendar">
-        <div class="row">
-            <div class="col s12 m4 l3">
-                {{--<div id='external-events'>
-                    <h4 class="header">Draggable Events</h4>
-                    <div class='fc-event cyan'>March Invoices</div>
-                    <div class='fc-event teal'>Call Emy</div>
-                    <div class='fc-event cyan darken-1'>Dinner with Team</div>
-                    <div class='fc-event cyan accent-4'>Meeting with Support Team</div>
-                    <div class='fc-event teal accent-4'>Meeting with Sales Team</div>
-                    <div class='fc-event light-blue accent-3'>Design an iOS App</div>
-                    <div class='fc-event light-blue accent-4'>Company Party</div>
-                    <p>
-                        <input type='checkbox' id='drop-remove' />
-                        <label for='drop-remove'>remove after drop</label>
-                    </p>
-                </div>--}}
-            </div>
-            <div class="col s12 m8 l9">
-                <div id='calendar'></div>
+    <div class="container">
+        <div class="section">
+            {{--<p class="caption">Info. de todos los comercios y buscador</p>
+            <div class="divider"></div>--}}
+            <div id="full-calendar">
+                <div class="row">
+                    <div class="col s12 m8 l9">
+                        <div id='calendar'></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
 @endsection
 
 @section('_footer')
@@ -65,9 +56,7 @@
     <script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/new/fullcalendar.min.js') !!}"></script>
     <script type="text/javascript" src="{!! asset('js/plugins/fullcalendar/new/locale-all.js') !!}"></script>
     <script>
-
-
-        $(document).ready(function() {
+        $(document).ready(function () {
             var initialLocaleCode = 'es';
 
             $('#calendar').fullCalendar({
@@ -142,7 +131,7 @@
             });
 
             // build the locale selector's options
-            $.each($.fullCalendar.locales, function(localeCode) {
+            $.each($.fullCalendar.locales, function (localeCode) {
                 $('#locale-selector').append(
                     $('<option/>')
                         .attr('value', localeCode)
@@ -152,7 +141,7 @@
             });
 
             // when the selected option changes, dynamically change the calendar option
-            $('#locale-selector').on('change', function() {
+            $('#locale-selector').on('change', function () {
                 if (this.value) {
                     $('#calendar').fullCalendar('option', 'locale', this.value);
                 }
